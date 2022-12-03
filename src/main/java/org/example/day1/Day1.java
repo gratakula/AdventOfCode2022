8428cbcb3bd635bc68a45b24fede396333ee6cb4
user <suckmyass@bitch.com>
2022-12-03 21:51:21 +0300

    Day 1 subtask 2

--- src/main/java/org/example/day1/Day1.java
+++ src/main/java/org/example/day1/Day1.java
@@ -4,17 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 
-public class MainApp {
+public class Day1 {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-        try (Stream<String> lines = Files.lines(Path.of("./src/main/java/org/example/day1/input"))) {
+        List<Integer> sums = sumItUp(Path.of("./src/main/java/org/example/day1/input"));
+        sums.sort((x, y) -> y - x);
+
+        System.out.println(sums.get(0));
+        System.out.println(sums.subList(0, 3).stream().mapToInt(Integer::intValue).sum());
+    }
+
+    public static List<Integer> sumItUp(Path path) {
+        List<Integer> sums = new ArrayList<>();
+        try (Stream<String> lines = Files.lines(path)) {
             List<String> linesList = lines.toList();
-            List<Integer> sums = new ArrayList<>();
             int total = 0;
             for (String line : linesList) {
                 if (!line.isEmpty()) {
@@ -25,11 +31,9 @@ public class MainApp {
                 }
             }
             sums.add(total);
-
-            System.out.println(Collections.max(sums));
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return sums;
     }
 }
